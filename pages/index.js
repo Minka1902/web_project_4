29dff362a2cbdbc2f0633c138fe25c7df1aea0f6
Michael Scharff <minka.scharff@gmail.com>
2021-12-26 16:42:24 +0200

    26/12 16:42

--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,26 @@ let editButton = document.querySelector(".profile__edit-button");
 let popupWindow = document.querySelector(".popup");
 let closeButton = document.querySelector(".popup__close-button")
 let formElement = document.querySelector(".popup__form");
+let nameInput = document.getElementById("popupname");
+let jobInput = document.getElementById("popupaboutme");
+let profileName = document.getElementById("profilename");
+let profileAboutMe = document.getElementById("profiledescription");
 
 function togglePopupWindow() {
     if (!popupWindow.classList.contains("popup_opened")) {
-        popupWindow.classList.add("popup_opened")
+        popupWindow.classList.add("popup_opened");
     } else {
-        popupWindow.classList.remove("popup_opened")
+        popupWindow.classList.remove("popup_opened");
+        nameInput.value = profileName.textContent;
+        jobInput.value = profileAboutMe.textContent;
     }
 }
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
-    let nameInput = document.getElementById("popupname");
-    let jobInput = document.getElementById("popupaboutme");
 
     let nameInputText = nameInput.value;
     let jobInputText = jobInput.value;
-
-    let profileName = document.getElementById("profilename");
-    let profileAboutMe = document.getElementById("profiledescription");
 
     profileName.textContent = nameInputText;
     profileAboutMe.textContent = jobInputText;
